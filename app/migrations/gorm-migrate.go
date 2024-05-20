e7f9d23ad0cff14aefad312a07bf5be11909af61
Eza <ezabintangramadhan@gmail.com>
2024-05-20 16:21:25 +0700

    fix:automigrate

--- app/migrations/gorm-migrate.go
+++ app/migrations/gorm-migrate.go
@@ -3,9 +3,10 @@ package migrations
 import (
 	"github.com/KELOMPOK-1-AIRBNB/BE-AIRBNB/app/configs"
 	"github.com/KELOMPOK-1-AIRBNB/BE-AIRBNB/app/databases"
+	homestayData "github.com/KELOMPOK-1-AIRBNB/BE-AIRBNB/features/homestays/data"
 	userData "github.com/KELOMPOK-1-AIRBNB/BE-AIRBNB/features/user/data"
 )
 
 func InitialMigration() {
-	databases.InitDBMysql(configs.InitConfig()).AutoMigrate(&userData.User{})
+	databases.InitDBMysql(configs.InitConfig()).AutoMigrate(&userData.User{}, &homestayData.Homestay{})
 }
